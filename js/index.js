c38885718647b4fd636d9d7e95617e506610a14d
jbnelson0 <james.bnelson0@gmail.com>
2017-07-12 14:39:00 -0400

    start to rewrite website

---
+++ js/index.js
@@ -0,0 +1,25 @@
+// import * as firebase from 'firebase';
+
+// const Storage = {}
+
+// // Initialize Firebase
+// const config = {
+//   apiKey: "AIzaSyAduSrhoe5beJ-Wtta5RLAVuYgAVLeLCIo",
+//   authDomain: "personalwebsite-f6275.firebaseapp.com",
+//   databaseURL: "https://personalwebsite-f6275.firebaseio.com",
+//   projectId: "personalwebsite-f6275",
+//   storageBucket: "",
+//   messagingSenderId: "658454691719"
+// };
+
+// firebase.initializeApp(config);
+
+// const storage = firebase.storage();
+
+// Storage.getPhoto = () => {
+//     const image = storage.ref('pictures/photo1.jpg').getDownloadURL();
+
+//     console.log(image, 'in getPhoto')
+// }
+
+// module.exports = Storage
